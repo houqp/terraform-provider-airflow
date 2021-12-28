696a15c34c3ff5ecb4f4fbd82a796b108d7ad531
Ilia Lazebnik <Ilia.lazebnik@gmail.com>
2021-12-28 16:07:38 +0200

    Update demo.tf

--- examples/variable/demo.tf
+++ examples/variable/demo.tf
@@ -1,16 +1,15 @@
 terraform {
   required_providers {
     airflow = {
-      source  = "houqp/airflow"
-      version = "0.2.1"
+      source  = "drfaust92/airflow"
+      version = "0.2.9"
     }
   }
 }
 
 provider "airflow" {
   base_endpoint = "http://localhost:28080/"
-  username      = "test"
-  password      = "test"
+  oauth2_token  = "some-token"
 }
 
 resource "airflow_variable" "foo" {
